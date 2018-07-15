931de47686112220f19e35212f2790cc2592c532
David Eini <DavidE@call.local>
2018-07-15 15:13:41 +0300

    sync

---
+++ src/app/pages/auth-page/auth-page.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthPageComponent } from './auth-page.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AuthPageComponent]
+})
+export class AuthPageModule { }
--- src/pages/auth/auth.ts
+++ src/pages/auth/auth.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the TestPage page.
+ * Generated class for the AuthPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -10,16 +10,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-test',
-  templateUrl: 'test.html',
+  selector: 'page-auth',
+  templateUrl: 'auth.html',
 })
-export class TestPage {
+export class AuthPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad TestPage');
+    console.log('ionViewDidLoad AuthPage');
   }
 
 }
